26ee468e53d16c0af5726d8a8aab1796b0ae0163
Adekurp <Adekurp@gmail.com>
2023-10-03 16:56:39 +0700

    Reporting Feature

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::get('purchase-order-lines/{id}/destroy', 'PurchaseOrdercontroller@getPurchaseOrderLineDestroy')->name('purchase.order.lines.destroy');
     Route::post('purchase-order-lines/{id}/update', 'PurchaseOrdercontroller@getPurchaseOrderLineUpdate')->name('purchase.order.lines.update');
     Route::post('purchase-order-lines/create', 'PurchaseOrdercontroller@getPurchaseOrderLineInsert')->name('purchase.order.lines.insert');
-
+    Route::get('reporting', 'DashboardController@getReportingPage')->name('reporting-page');
+    Route::get('reporting/all-data-product', 'DashboardController@getAllDataProduct')->name('reporting.all.data.product');
+    Route::get('reporting/chart-product', 'DashboardController@getChartProduct')->name('reporting.chart.product');
 });
 
 
